318f089ed16f362789d6f552a81d6e3a527b820a
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2020-03-09 13:41:50 -0300

    aula jm

--- dsc-workspace/aula01/src/aula01/TestMain.java
+++
@@ -1,8 +0,0 @@
-package aula01;
-
-public class TestMain {
-
-	public static void main(String[] args) {
-		System.out.println("Teste Main");
-	}
-}
---
+++ seminario/sorteio.py
@@ -0,0 +1,8 @@
+
+import random
+
+linguagens = ["C#","Clojure","Erlang","Elixir","Fortress","Go (Google)","Haskell","Julia","Kotlin","Lua","ML","Concurrency Pascal","Ruby","Swift (Parallel)","Scala","ECMAScript","Python"]
+
+x = random.randint(0,len(linguagens)-1)
+
+print linguagens[x]
